cca550f42132b3b57faff70d5ced8d09bc9379b3
Vinicius Benites <viniciusbst28@outlook.com>
2024-11-25 23:02:22 -0300

    Fix sintaxe Hello World DAG

--- dags/hello.py
+++ dags/hello.py
@@ -3,27 +3,17 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'benites'
-    'start_date': datetime(year: 2024, month: 11, day:25)
-    'catchup': False
+    "owner": "benites",
+    "start_date": datetime(2024, 1, 25),
+    "catchup": False,
 }
 
-dag = DAG(
-    dag_id: 'hello_world'
-    default_args = default_args,
-    schedule=timedelta(days=1)
-)
+dag = DAG("hello_world", default_args=default_args, schedule=timedelta(days=1))
 
-t1 = BashOperator(
-    task_id = 'hello_world'
-    bash_command='echo "Hello World"'
-    dag = dag
-)
+t1 = BashOperator(task_id="hello_world", bash_command='echo "Hello World"', dag=dag)
 
 t2 = BashOperator(
-    task_id = 'hello_world'
-    bash_command='echo "Hello World 2"'
-    dag = dag
+    task_id="hello_dml", bash_command='echo "Hello Data Mastery Lab"', dag=dag
 )
 
 t1 >> t2
